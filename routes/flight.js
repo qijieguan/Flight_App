1c688d624b8901dc5f73701f8fa5b77e2e6984d3
qijieguan <qijieguan7@gmail.com>
2023-08-22 19:17:35 -0700

    testing deploy site

--- routes/flight.js
+++ routes/flight.js
@@ -67,7 +67,7 @@ router.route('/search-flight').post(async (req, res) => {
     }
     
     await axios.request(options(method, url, params, headers))
-    .then((response) => { res.json(response.data); });
+    .then((response) => { res.send(response); });
   }
   catch (error) {
     console.error(error);
